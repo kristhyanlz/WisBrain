6f274e037d6d7f604eb486bc2610f1a2ff1cbf54
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-06-01 20:21:07 -0500

    arduino update

--- Arduino/Arduino_v2_analog/Arduino_v2_analog.ino
+++ Arduino/Arduino_v2_analog/Arduino_v2_analog.ino
@@ -7,7 +7,7 @@ char sensorName[] = {'A', 'B', 'C', 'D'};
 int led_rojo  = 3;
 int led_verde = 2;
 
-int time_delay = 0;
+int led_time = 0;
 
 bool state = false;
 
@@ -15,38 +15,45 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  pinMode(led_rojo, INPUT);
+  pinMode(led_rojo, OUTPUT);
   pinMode(led_verde, INPUT);
   //ANALOG: 0 - 1023
   //204
   
-  //for(register int i = 0; i < N_SENSORS;++i)
-  //  pinMode(sensorPin[i], INPUT);
-  time_delay = millis();
+  led_time = millis();
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int time_now = millis();
 
-  for (register int i = 0; i < N_SENSORS; ++i){
-    int actualState = (analogRead(sensorPin[i]) > 512)? 1 : 0;
-    if (sensorState[i] != actualState){
-      Serial.print(sensorName[i]);
-      Serial.println(actualState);
-
-      sensorState[i] = actualState;
+  if (Serial.available() > 0){
+    char msg = Serial.read();
+    led_time = millis();
+    switch (msg){
+      case '0':
+        digitalWrite(led_rojo, HIGH);
+        break;
+      case '1':
+        digitalWrite(led_verde, HIGH);
+        break;
     }
   }
 
-  if ((time_now - time_delay) >= 1000){
-    time_delay = millis();
+  // FOR detecta tarjeta
+    for (register int i = 0; i < N_SENSORS; ++i){
+      int actualState = (analogRead(sensorPin[i]) > 512)? 1 : 0;
+      if (sensorState[i] != actualState){
+        Serial.print(sensorName[i]);
+        Serial.println(actualState);
 
-    digitalWrite(led_rojo, (state==true)? HIGH : LOW);
-    digitalWrite(led_verde, (state==true)? LOW : HIGH);
+        sensorState[i] = actualState;
+      }
+    }
 
-    state = not state;
+  if ( (time_now - led_time) >= 1500 ){
+    digitalWrite(led_rojo,  LOW);
+    digitalWrite(led_verde, LOW);
   }
   
   delay(1);
