9b366a972db2eee2452dd58ad25a1a549bd166fe
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-06-06 19:08:23 -0500

    Limpiando todo

--- WisBrain_Back/Tarjetas.py
+++
@@ -1,6 +0,0 @@
-import tarjetas_bd
-
-class Tarjetas:
-    def __init__(self):
-        self.tarjetasBase = tarjetas_bd.devolverTarjetasBase()
-        self.tarjetasPaciente = tarjetas_bd.devolverTarjetasOriginales()
